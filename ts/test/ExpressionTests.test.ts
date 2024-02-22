1470a03950bf002cd070e20f6dd1517677c85aef
Marcin Szwarc <marcinszwarc@hotmail.com>
2024-02-22 17:39:57 +0100

    Test the evaluation

---
+++ ts/test/ExpressionTests.test.ts
@@ -0,0 +1,32 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { Parser } from '../src/parser/Parser.js';
+import { Tokenizer } from '../src/parser/Tokenizer.js';
+import { EvaluatedTreeNode } from '../src/evaluator/EvaluatedTreeNode.js';
+import { EvaluationContext } from '../src/evaluator/EvaluationContext.js';
+import { NodeEvaluator } from '../src/evaluator/NodeEvaluator.js';
+import { assert } from 'chai';
+
+describe('Expressions from .t files', () => {
+    // Read files with .t extension from the /parserTests folder
+    // And treat the content of every file as an expression that should be truthy
+
+    const testFolder = 'parserTests';
+
+    fs.readdirSync(testFolder).forEach((file: string) => {
+        if (path.extname(file) === '.t') {
+            it(`should evaluate ${file} as a truthy expression`, async () => {
+                const content = fs.readFileSync(path.join(testFolder, file), 'utf8');
+                const tokenizer = new Tokenizer();
+                const parser = new Parser();
+                const tokens = tokenizer.tokenize(content);
+                const rootNode = parser.parse(tokens)!;
+                const evaluatedRootNode = new EvaluatedTreeNode(rootNode);
+                await evaluatedRootNode.evaluate(new EvaluationContext(), new NodeEvaluator());
+
+                assert.isTrue(evaluatedRootNode.wasEvaluated);
+                assert.isTrue(evaluatedRootNode.value.isTruthy());
+            });
+        }
+    });
+});
---
+++ ts/test/evaluator/NodeEvaluator.test.ts
@@ -0,0 +1,81 @@
+import { assert } from 'chai';
+import { Tokenizer } from '../../src/parser/Tokenizer.js';
+import { Parser } from '../../src/parser/Parser.js';
+import { EvaluatedTreeNode } from '../../src/evaluator/EvaluatedTreeNode.js';
+import { NodeEvaluator } from '../../src/evaluator/NodeEvaluator.js';
+import { EvaluationContext } from '../../src/evaluator/EvaluationContext.js';
+import { ValueDataType } from '../../src/evaluator/ValueDataType.js';
+
+describe('Evaluator tests', () => {
+    describe('AND operator', () => {
+        it('should evaluate true & true as true', async () => {
+            const tokens = (new Tokenizer()).tokenize('true & true');
+            const rootNode = (new Parser()).parse(tokens)!;
+            const evaluatedRootNode = new EvaluatedTreeNode(rootNode);
+            await evaluatedRootNode.evaluate(new EvaluationContext(), new NodeEvaluator());
+
+            assert.isTrue(evaluatedRootNode.wasEvaluated);
+
+            const value = evaluatedRootNode.value;
+
+            assert.equal(value.dataType, ValueDataType.Boolean);
+            assert.equal(value.value, true);
+        });
+
+        it('should evaluate false & true as false', async () => {
+            const tokens = (new Tokenizer()).tokenize('false & true');
+            const rootNode = (new Parser()).parse(tokens)!;
+            const evaluatedRootNode = new EvaluatedTreeNode(rootNode);
+            await evaluatedRootNode.evaluate(new EvaluationContext(), new NodeEvaluator());
+
+            assert.isTrue(evaluatedRootNode.wasEvaluated);
+
+            const value = evaluatedRootNode.value;
+
+            assert.equal(value.dataType, ValueDataType.Boolean);
+            assert.equal(value.value, false);
+        });
+
+        it('should evaluate false & false as false', async () => {
+            const tokens = (new Tokenizer()).tokenize('false & false');
+            const rootNode = (new Parser()).parse(tokens)!;
+            const evaluatedRootNode = new EvaluatedTreeNode(rootNode);
+            await evaluatedRootNode.evaluate(new EvaluationContext(), new NodeEvaluator());
+
+            assert.isTrue(evaluatedRootNode.wasEvaluated);
+
+            const value = evaluatedRootNode.value;
+
+            assert.equal(value.dataType, ValueDataType.Boolean);
+            assert.equal(value.value, false);
+        });
+
+        it('should evaluate 0 & true as 0', async () => {
+            const tokens = (new Tokenizer()).tokenize('0 & true');
+            const rootNode = (new Parser()).parse(tokens)!;
+            const evaluatedRootNode = new EvaluatedTreeNode(rootNode);
+            await evaluatedRootNode.evaluate(new EvaluationContext(), new NodeEvaluator());
+
+            assert.isTrue(evaluatedRootNode.wasEvaluated);
+
+            const value = evaluatedRootNode.value;
+
+            assert.equal(value.dataType, ValueDataType.Integer);
+            assert.equal(value.value, 0);
+        });
+
+        it('should evaluate true & 0 as false', async () => {
+            const tokens = (new Tokenizer()).tokenize('true & 0');
+            const rootNode = (new Parser()).parse(tokens)!;
+            const evaluatedRootNode = new EvaluatedTreeNode(rootNode);
+            await evaluatedRootNode.evaluate(new EvaluationContext(), new NodeEvaluator());
+
+            assert.isTrue(evaluatedRootNode.wasEvaluated);
+
+            const value = evaluatedRootNode.value;
+
+            assert.equal(value.dataType, ValueDataType.Boolean);
+            assert.equal(value.value, false);
+        });
+    });
+});
