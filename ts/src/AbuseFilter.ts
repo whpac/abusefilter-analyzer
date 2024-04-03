7811f8c584b683733d7d88b0b31329015ffd5aef
Marcin Szwarc <marcinszwarc@hotmail.com>
2024-04-03 21:06:20 +0200

    Create tree walker

--- ts/src/AbuseFilter.ts
+++ ts/src/AbuseFilter.ts
@@ -1,6 +1,7 @@
 import { EvaluationContext } from './evaluator/EvaluationContext.js';
 import { NodeEvaluator } from './evaluator/NodeEvaluator.js';
 import { EvaluableNodeFactory } from './evaluator/nodes/EvaluableNodeFactory.js';
+import { IEvaluationContext } from './model/IEvaluationContext.js';
 import { IEvaluableTreeNode } from './model/nodes/IEvaluableTreeNode.js';
 import { IValue } from './model/value/IValue.js';
 import { Parser } from './parser/Parser.js';
@@ -11,17 +12,36 @@ export class AbuseFilter {
     public readonly tokens: readonly Token[];
     public readonly rootNode: IEvaluableTreeNode;
 
+    protected defaultContext: IEvaluationContext;
+
     public constructor(filterText: string) {
         const tokenizer = new Tokenizer();
         const parser = new Parser(new EvaluableNodeFactory());
 
         this.tokens = tokenizer.tokenize(filterText);
         this.rootNode = parser.parse(this.tokens);
+
+        this.defaultContext = new EvaluationContext();
     }
 
-    public async evaluate(): Promise<IValue> {
+    public async evaluate(context?: IEvaluationContext): Promise<IValue> {
         const evaluator = new NodeEvaluator();
-        const context = new EvaluationContext();
+        context ??= this.defaultContext;
         return await evaluator.evaluateNode(this.rootNode, context);
     }
+
+    public walkTree(callback: TreeWalkerCallback, context?: IEvaluationContext): void {
+        this.walkTreeInner(this.rootNode, 0, callback, context);
+    }
+
+    protected walkTreeInner(node: IEvaluableTreeNode, currentDepth: number, callback: TreeWalkerCallback, context?: IEvaluationContext): void {
+        context ??= this.defaultContext;
+        const value = node.getValue(context);
+        callback(node, value, currentDepth);
+        for(const child of node.children) {
+            this.walkTreeInner(child, currentDepth + 1, callback, context);
+        }
+    }
 }
+
+type TreeWalkerCallback = (node: IEvaluableTreeNode, value: IValue, depth: number) => void;
