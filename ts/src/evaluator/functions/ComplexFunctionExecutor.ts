8025005fe5939c321c9cd9793fcf7c90276043dd
Marcin Szwarc <marcinszwarc@hotmail.com>
2024-05-14 21:00:33 +0200

    Added complex function executor

---
+++ ts/src/evaluator/functions/ComplexFunctionExecutor.ts
@@ -0,0 +1,22 @@
+import { IEvaluationContext } from '../../model/IEvaluationContext.js';
+import { IValue } from '../../model/value/IValue.js';
+import { IFunctionExecutor } from './IFuctionExecutor.js';
+
+export class ComplexFunctionExecutor implements IFunctionExecutor {
+    protected readonly executors: IFunctionExecutor[];
+
+    public constructor(executors: IFunctionExecutor[]) {
+        this.executors = executors;
+    }
+
+    public async executeFunction(functionName: string, context: IEvaluationContext, args: IValue[]): Promise<IValue> {
+        for (const executor of this.executors) {
+            try {
+                return await executor.executeFunction(functionName, context, args);
+            } catch (e) {
+                // Ignore and try the next executor
+            }
+        }
+        throw new Error(`Function ${functionName} is unknown or cannot be executed`);
+    }
+}
