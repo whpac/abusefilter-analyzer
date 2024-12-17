3026ac7602d7a4483f6014fcea26a98a19681bfd
Marcin Szwarc <marcinszwarc@hotmail.com>
2024-12-17 21:42:51 +0100

    Support MediaWiki night mode

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -36,15 +36,41 @@ function main() {
 main();
 
 mw.util.addCSS(`
+.afa-tree-container {
+    --afa-color-value-keyword: blue;
+    --afa-color-value-string: brown;
+    --afa-color-value-number: darkgreen;
+    --afa-color-value-identifier: teal;
+    --afa-color-keyword: purple;
+    --afa-color-function: #8f6300;
+    --afa-color-true: green;
+    --afa-color-false: red;
+}
+
 .afa-value, .afa-token { font-family: monospace; }
-.afa-value-keyword { color: blue; }
-.afa-value-string { color: brown; }
-.afa-value-number { color: darkgreen; }
-.afa-value-identifier { color: teal; }
-.afa-keyword { color: purple; }
-.afa-function { color: #8f6300; }
-.afa-hint { display: inline-block; background: #eee; padding: 0 0.5em; border-radius: 0.5em; font-size:0.85em; color:#666; }
-.afa-data { margin-left: 0.7em; display: inline-block; border: 1px solid #aaa; padding: 0 0.5em; border-radius: 0.5em; background: #f4f4f4; font-size: 0.85em; }
+.afa-value-keyword { color: var(--afa-color-value-keyword); }
+.afa-value-string { color: var(--afa-color-value-string); }
+.afa-value-number { color: var(--afa-color-value-number); }
+.afa-value-identifier { color: var(--afa-color-value-identifier); }
+.afa-keyword { color: var(--afa-color-keyword); }
+.afa-function { color: var(--afa-color-function); }
+.afa-hint {
+    display: inline-block;
+    background: var(--background-color-neutral, #eaecf0);
+    padding: 0 0.5em;
+    border-radius: 0.5em;
+    font-size: 0.85em;
+    color: var(--color-base--subtle, #54595d);
+}
+.afa-data {
+    margin-left: 0.7em;
+    display: inline-block;
+    border: 1px solid var(--border-color-base, #a2a9b1);
+    padding: 0 0.5em;
+    border-radius: 0.5em;
+    background: var(--background-color-neutral, #eaecf0);
+    font-size: 0.85em;
+}
 
 .afa-data .afa-value-bool::before {
     content: '';
@@ -55,16 +81,16 @@ mw.util.addCSS(`
     background: #999;
     margin-right: 0.3em;
 }
-.afa-data .afa-value-true::before { background: green; }
-.afa-data .afa-value-false::before { background: red; }
+.afa-data .afa-value-true::before { background: var(--afa-color-true); }
+.afa-data .afa-value-false::before { background: var(--afa-color-false); }
 
 .afa-data .afa-data-more {
     position: absolute;
     margin-top: -0.3em;
     left: 1em;
     right: 1em;
-    background: #f0f0f0;
-    border: 1px solid #aaa;
+    background: var(--background-color-neutral, #eaecf0);
+    border: 1px solid var(--border-color-base, #a2a9b1);
     padding: 0.5em;
     border-radius: 0.5em;
     display: none;
@@ -75,11 +101,11 @@ mw.util.addCSS(`
 .afa-data-error:not(.afa-data-error-speculative) { color: red; font-weight: bold; }
 
 .afa-tree-container {
-    border: 1px solid #ccc;
+    border: 1px solid var(--border-color-subtle, #c8ccd1);
     padding: 0.5em;
     margin: 0.5em 0;
     border-radius: 0.25em;
-    background: #fcfcfc;
+    background: var(--background-color-neutral-subtle, #f8f9fa);
 }
 
 .afa-tree-container ul {
@@ -90,5 +116,29 @@ mw.util.addCSS(`
 
 .afa-tree-container ul:has(> li > details) > li:not(:has(> details)) {
     padding-left: 1em;
+}
+
+html.skin-theme-clientpref-night .afa-tree-container {
+    --afa-color-value-keyword: #749afa;
+    --afa-color-value-string: #e08870;
+    --afa-color-value-number: #b6f2b6;
+    --afa-color-value-identifier: #a8e7ff;
+    --afa-color-keyword: #e173ff;
+    --afa-color-function: #ffd77e;
+    --afa-color-true: #0d0;
+    --afa-color-false: #f33;
+}
+
+@media screen and (prefers-color-scheme: dark) {
+    html.skin-theme-clientpref-os .afa-tree-container {
+        --afa-color-value-keyword: #749afa;
+        --afa-color-value-string: #e08870;
+        --afa-color-value-number: #b6f2b6;
+        --afa-color-value-identifier: #a8e7ff;
+        --afa-color-keyword: #e173ff;
+        --afa-color-function: #ffd77e;
+        --afa-color-true: #0d0;
+        --afa-color-false: #f33;
+    }
 }
 `);
